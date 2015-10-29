b0a2a22c7d1eb2746d9858cca7db6ca97cc8503d
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegrogroup.com>
2015-10-29 11:04:28 +0100

    change flickr feed parsing

--- src/flickr.js
+++ src/flickr.js
@@ -1,30 +1,30 @@
-"use strict";
-
+/* jshint esnext: true */
+(function(){
+  "use strict";
 var http = require('http');
-var parser = require('xml2json');
-var flickrApiUrl = 'http://api.flickr.com/services/feeds/photos_public.gne?id=23364033@N07&format=xml&jsoncallback=f';
+var flickrApiUrl = 'http://api.flickr.com/services/feeds/photos_public.gne?id=23364033@N07&format=json&jsoncallback=%27%27';
 
-function parseFeed(body, limit = 100) {
-    var feed = JSON.parse(parser.toJson(body)).feed.entry;
+function parseFeed(flickrFeed, limit = 100) {
+    var feed = flickrFeed.items;
     return feed.map(a => {
-        var photo = a.link[1].href.replace('http', 'https');
+        var photo = a.media.m.replace('http', 'https');
         return {
             title: a.title,
             media: {
                 small: photo.substring(0, photo.length - 6) + '_q.jpg',
-                big: photo
+                big: photo.substring(0, photo.length - 6) + '_b.jpg',
             },
-            href: a.link[0].href
-        }
+            href: a.link.replace('http', 'https')
+        };
     }).slice(0, limit);
 }
 
 function render(feed) {
     var entries = feed.map(item => {
-        return `<li><a href="${item.href}" data-img="${item.media.big}"><img src="${item.media.small}"  alt="${item.title}"/></a></li>`
+        return `<li><a href="${item.href}" data-img="${item.media.big}"><img src="${item.media.small}"  alt="${item.title}"/></a></li>`;
     }).join('\n');
     return `<ul class="flickr-photos">${entries}</ul>`;
-};
+}
 
 http.get(flickrApiUrl, function(response) {
     var body = '';
@@ -32,7 +32,10 @@ http.get(flickrApiUrl, function(response) {
         body += d;
     });
     response.on('end', function() {
-             var entries = render(parseFeed(body, 6));
+      /* jshint -W061 */
+        var flickrFeed = eval(body);
+        var entries = render(parseFeed(flickrFeed, 6));
         process.stdout.write(entries);
     });
 });
+})();
