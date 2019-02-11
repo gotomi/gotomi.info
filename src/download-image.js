17783c7b9e5656f0a502eaa253f1115ec8070aeb
gotomi <gotomi@gotomi.info>
2019-02-11 19:11:16 +0100

    fix image conversion

--- src/download-image.js
+++ src/download-image.js
@@ -7,47 +7,49 @@ const sharp = require('sharp');
 
 const downloadImage = async (url, filename, dest) => {
     const placeToWrite = path.join(dest, filename);
+
+
     const response = await axios({
         method: 'get',
         url: url,
         responseType: 'stream'
     });
-    await response.data.pipe(fs.createWriteStream(placeToWrite))
+    await response.data.pipe(fs.createWriteStream(placeToWrite + '.jpg'))
+
+
+
+
+    const convertImages = function () {
+
+        const filename = placeToWrite;
+
+        sharp(placeToWrite + '.jpg')
+            .resize(200)
+            .jpeg()
+            .toFile(`${filename}-200.jpg`, (err, info) => {
+                //   console.log(info, err);
+            });
+
+
+        sharp(placeToWrite + '.jpg')
+            .resize(20)
+            .blur(1)
+            .jpeg({
+                quality: 10
+            })
+            .toFile(`${filename}-small.jpg`, (err, info) => {
+                //   console.log(info, err);
+            });
 
 
 
 
 
-    const convertImages = function(){
-        const filename = placeToWrite.slice(0,placeToWrite.lastIndexOf('.'));
-
-        sharp(placeToWrite)
-        .resize(200)
-        .jpeg()
-        .toFile(`${filename}-200.jpg`, (err, info) => {
-         //   console.log(info, err);
-        });
-
-
-        sharp(placeToWrite)
-        .resize(20)
-        .blur(1)
-        .jpeg({
-            quality: 10
-        })
-        .toFile(`${filename}-small.jpg`, (err, info) => {
-         //   console.log(info, err);
-        } );
-
-
-
-
-
-        sharp(placeToWrite)
-        .webp()
-        .toFile(`${filename}.webp`, (err, info) => {
-          //  console.log(info, err);
-        } )
+        sharp(placeToWrite + '.jpg')
+            .webp()
+            .toFile(`${filename}.webp`, (err, info) => {
+                //  console.log(info, err);
+            })
     }
 
 
