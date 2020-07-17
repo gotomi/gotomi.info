3c2ab9cf95aefc9747370bab0e460b898ee8370c
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2020-07-17 16:32:17 +0200

    instagram once again

--- src/instagram.js
+++ src/instagram.js
@@ -1,19 +1,20 @@
-const path = require('path');
-const home = require('os').homedir();
-
-var ig = require('instagram-scraping');
-
+const fetch = require('node-fetch');
+const instagramFeedUrl = 'https://www.instagram.com/gotomik/?__a=1';
 
 const getUserFeed = async function() {
-    const result = await ig.scrapeUserPage('gotomik');
-    const images = result.medias.map(item => {
-        return {
-            text: item.text,
-            id: item.media_id,
-            href: item.display_url,
-            url: item.thumbnail
-        }
-    });
+    const instagramPageUrl = 'https://www.instagram.com/p/';
+    const result = await fetch(instagramFeedUrl);
+    const json = await result.json();
+    const timeline = json.graphql.user.edge_owner_to_timeline_media.edges;
+    const images = timeline.map(item => {
+    return {
+        text: item.node.edge_media_to_caption.edges[0].node.text,
+        url: item.node.thumbnail_src,
+        id: item.node.id,
+        href: instagramPageUrl + item.node.shortcode
+    }
+})
+
     return images
 }
 
