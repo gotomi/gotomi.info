24e508be002aa310b39d023b8e02a3e26e1c0475
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2020-07-17 14:24:45 +0200

    scraping instagram photos fix

--- src/instagram.js
+++ src/instagram.js
@@ -1,52 +1,20 @@
 const path = require('path');
 const home = require('os').homedir();
-const Instagram = require('node-instagram').default;
 
-// Create a new instance.
+var ig = require('instagram-scraping');
 
-const getConfig = function () {
-    try {
-        return require(path.join(home, '/inst.json'));
-    } catch (e) {
+
+const getUserFeed = async function() {
+    const result = await ig.scrapeUserPage('gotomik');
+    const images = result.medias.map(item => {
         return {
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            accessToken: process.env.ACCESS_TOKEN
+            text: item.text,
+            id: item.media_id,
+            href: item.display_url,
+            url: item.thumbnail
         }
-    }
-}
-
-const config = getConfig();
-const instagram = new Instagram(config);
-
-var getUserFeed = async function () {
-    const feed = await instagram.get('users/self/media/recent');
-    let images = [];
-    feed.data.forEach(item => {
-
-        if (item.type === 'carousel') {
-            item.carousel_media.forEach((image, index) => {
-                images.push({
-                    text: item.caption.text,
-                    id: item.id + '-' + index,
-                    href: item.link,
-                    url: image.images.standard_resolution.url
-                })
-            })
-        } else if (item.type === 'image') {
-
-            images.push({
-                text: item.caption.text,
-                id: item.id,
-                href: item.link,
-                url: item.images.standard_resolution.url
-            })
-        }
-
-
     });
-
-    return images;
+    return images
 }
 
 module.exports = getUserFeed
