bc589efda9e529c529456e55fb0c554cd186d670
Tomasz Gospodarczyk <tomasz.gospodarczyk@allegro.pl>
2022-04-06 16:45:26 +0200

    astro ready to 1.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,10 @@
 import svelte from '@astrojs/svelte';
+// import sitemap from '@astrojs/sitemap';
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
-  buildOptions: {
-    site: 'http://gotomi.info/',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: false,         // Generate sitemap (set to "false" to disable)
-  },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-    // tailwindConfig: '',     // Path to tailwind.config.js if used, e.g. './tailwind.config.js'
-  },
-  integrations: [svelte()]
+    // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+    // pages: './src/pages', // Path to Astro components, pages, and data
+    // dist: './dist',       // When running `astro build`, path to final static output
+    // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+    site: 'http://gotomi.info/', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+    integrations: [svelte()]
 };
