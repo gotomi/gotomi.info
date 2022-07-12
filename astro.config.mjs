4bdc2e94a613ceddff5d77e08a18f6c18945e217
gotomi <gotomi@gotomi.info>
2022-07-12 09:21:59 +0200

    dependecies update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import deno from '@astrojs/deno';
 import svelte from '@astrojs/svelte';
 // import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
     integrations: [svelte()],
-    adapter: deno(),
     site: 'http://gotomi.info/',
   });
