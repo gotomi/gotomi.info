34de4ffc3f73ca696827034dda823132ae089904
gotomi <gotomi@gotomi.info>
2022-08-04 22:58:16 +0200

    gallery, playing with swipe

--- src/components/PhotoGallerySwipe.svelte
+++ src/components/PhotoGallerySwipe.svelte
@@ -1,48 +1,73 @@
 <script>
-  export let photos = [];
+  import { Swipe, SwipeItem } from "svelte-swipe";
 
-  let activeIndex = 0;
-  let activeItem = "";
+  export let photos = [];
+  let active_item = 0; //readonly
+  let SwipeComp;
+
   function setActive(index) {
     if (index < 0 || index >= photos.length) return;
-    activeIndex = index;
-    activeItem = photos[index];
+    SwipeComp.goTo(index);
   }
-
-  setActive(0);
 
   let key = 0;
   function handleKeydown(event) {
     key = event.key;
     if (key === "ArrowLeft") {
-      setActive(activeIndex - 1);
+      setActive(active_item - 1);
     } else if (key === "ArrowRight") {
-      setActive(activeIndex + 1);
+      setActive(active_item + 1);
+    }
+  }
+
+  const swipeConfig = {
+    autoplay: false,
+    delay: 2000,
+    showIndicators: true,
+    transitionDuration: 1000,
+    defaultIndex: 0,
+  };
+
+  function slideChange({ detail }) {
+    const index = detail.active_item;
+    const images = document.querySelectorAll(".featured img");
+    if (images[index].src !== photos[index].normal) {
+      images[index].src = photos[index].normal;
+      images[index].className="";
+    }
+
+    if (photos.length - 1 > index) {
+      images[index + 1].src = photos[index + 1].normal;
+      images[index + 1].className="";
     }
   }
 </script>
 
 <svelte:window on:keydown={handleKeydown} />
-{#if activeItem}
-  <div class="featured">
-    <span class="prev" on:click={() => setActive(activeIndex - 1)}
-      ><span>⇦</span></span
-    >
-    <span class="next" on:click={() => setActive(activeIndex + 1)}
-      ><span>⇨</span></span
-    >
-    <img
-      src={activeItem.normal}
-      style={`aspect-ratio:${activeItem.width}/${activeItem.height}`}
-      alt={activeItem.alt}
-      title={JSON.stringify(activeItem.exif)}
-    />
-  </div>
-{/if}
+
+<div class="featured">
+  <!-- <span class="prev" on:click={() => setActive(active_item - 1)}
+    ><span>⇦</span></span
+  >
+  <span class="next" on:click={() => setActive(active_item + 1)}
+    ><span>⇨</span></span
+  > -->
+  <Swipe bind:active_item bind:this={SwipeComp} on:change={slideChange}>
+    {#each photos as item, i}
+      <SwipeItem>
+        <img src={i < 2 ? item.normal : item.thumbnail} class={i < 1 ? '' : 'hide'} alt={item.alt} />
+      </SwipeItem>
+    {/each}
+  </Swipe>
+</div>
+
 <div class="photo-gallery-container">
   <ul class="photo-gallery">
-    {#each photos as item, index}
-      <li on:click={() => setActive(index)}>
+    {#each photos as item, i}
+      <li
+        on:click={() => setActive(i)}
+        class={active_item == i ? "active" : ""}
+      >
         <img
           src={item.thumbnail}
           alt={item.alt}
@@ -71,6 +96,9 @@
   .photo-gallery li {
     flex: 1 0 50px;
   }
+  .photo-gallery li.active {
+    border: 5px solid rgb(18, 1, 1);
+  }
   .photo-gallery-container {
     /* position: fixed; */
     bottom: 0;
@@ -98,12 +126,16 @@
     border-radius: 0.5rem;
     vertical-align: middle;
   }
+  .featured img.hide{
+    visibility: hidden;
+  }
 
   .next,
   .prev {
     position: absolute;
     top: 0;
     bottom: 0;
+    z-index: 999;
   }
   .next {
     right: 0;
