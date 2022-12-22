f6ea3eeb00186b89ad57102ed1bd77b7f513d60d
gotomi <gotomi@gotomi.info>
2022-12-22 23:32:05 +0100

    update

--- src/components/PhotoSwipe.svelte
+++ src/components/PhotoSwipe.svelte
@@ -10,26 +10,44 @@
   }
 </script>
 
-<div class="container" id="photos">
-  {#each photos as item, index}
-    <img src={item.url} alt={item.title} id="{'item' + index}" />
-  {/each}
+<div class="wrapper">
+  <div class="counter">1 / {photos.length}</div>
+
+  <div class="container" id="photos">
+    {#each photos as item, index}
+      <img src={item.url} alt={item.title} id={"item" + index} />
+    {/each}
+  </div>
 </div>
 
 <style>
-  .container {
+  .wrapper {
+    position: relative;
+    width: min(900px, 100vw);
     margin: auto;
+  }
+  .container {
     display: flex;
     white-space: nowrap;
     overflow-x: auto;
     overflow-y: hidden;
     scroll-snap-type: x mandatory;
     scrollbar-width: none;
-    width: min(900px, 100vw)
   }
   .container img {
     flex: 1;
     scroll-snap-align: start;
-    max-width: min(900px, 100vw)
+    max-width: min(900px, 100vw);
+  }
+  .counter {
+    background: #0009;
+    color: #fff9;
+    font-size: 13px;
+    padding: 0px 6px;
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    z-index: 2;
+    border-radius: 3px;
   }
 </style>
