a4356a7313ce9854249f1e78a2099ede41ad55be
gotomi <gotomi@gotomi.info>
2023-12-12 11:14:53 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
 // import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-    integrations: [svelte()],
-    site: 'http://gotomi.info/',
-  });
+  integrations: [svelte(), react()],
+  site: "http://gotomi.info/",
+});
